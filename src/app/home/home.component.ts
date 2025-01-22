6f09da1a1d9c2d31ea7f67244526e3074741199f
sanjay8169 <yadavsam975@gmail.com>
2025-01-22 11:59:09 -0800

    Fixed nav toggle bug and added sum images on home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,19 +28,19 @@ export class HomeComponent implements OnInit{
   ngOnInit(): void {
     this.slides[0] = {
       id: 0,
-      src: 'https://via.placeholder.com/800x400?text=Chemical+Image+1',
+      src: '/images/compressed/PSX_20241222_235143.jpg',
       title: 'First slide',
       subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
     };
     this.slides[1] = {
       id: 1,
-      src: 'https://via.placeholder.com/800x400?text=Chemical+Image+2',
+      src: 'images/compressed/PSX_20241222_234748.jpg',
       title: 'Second slide',
       subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     };
     this.slides[2] = {
       id: 2,
-      src: 'https://via.placeholder.com/800x400?text=Chemical+Image+3',
+      src: 'images/compressed/20241123_120341.jpg',
       title: 'Third slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
     };
