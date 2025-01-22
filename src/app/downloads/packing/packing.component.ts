28a8e785e2be67d150a2ec8354b1a12f327abe5f
sanjay8169 <yadavsam975@gmail.com>
2025-01-22 10:20:46 -0800

    Update the excel sheet json

--- src/app/downloads/packing/packing.component.ts
+++ src/app/downloads/packing/packing.component.ts
@@ -25,12 +25,97 @@ export class PackingComponent {
         alt: 'Image 2'
       },
       {
-        image: 'https://via.placeholder.com/800x400?text=Image+3',
+        image: 'images/chemicals-packing/20241123_120341.jpg',
         thumbnail: 'https://via.placeholder.com/100x50?text=Image+3',
         alt: 'Image 3'
       },
       {
-        image: 'https://via.placeholder.com/800x400?text=Image+4',
+        image: 'images/chemicals-packing/20241222_194423.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241222_194649.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241222_221211.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241222_221809.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241222_223044.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241222_223412.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241222_223539.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241222_235721.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/20241223_191359.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_202414.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_232833.png',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_233920.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_234309.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_234621.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_234715.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_234748.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_234948.jpg',
+        thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
+        alt: 'Image 4'
+      },
+      {
+        image: 'images/chemicals-packing/PSX_20241222_235040.jpg',
         thumbnail: 'https://via.placeholder.com/100x50?text=Image+4',
         alt: 'Image 4'
       }
